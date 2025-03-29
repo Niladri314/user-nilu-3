8d16597c6d5a42a18c41a8506d6f100b30460b87
nilu <ndebankura01@gmail.com>
2025-03-29 16:15:36 +0530

    first commit

---
+++ src/main/java/com/examplexyz/ExamplexyzApplication.java
@@ -0,0 +1,13 @@
+package com.examplexyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplexyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplexyzApplication.class, args);
+	}
+
+}
