1c9bcdbd4f7d8f0e6f027ac7dbdf8b001d59c8d7
nilu <ndebankura01@gmail.com>
2025-03-29 16:51:02 +0530

    first commit

--- src/main/java/com/examplexyz/p1/A.java
+++ src/main/java/com/examplexyz/p1/A.java
@@ -2,6 +2,8 @@ package com.examplexyz.p1;
 
 public class A {
     public static void main(String[] args) {
+
         System.out.println("statement 1");
+        System.out.println("statement 2");
     }
 }
